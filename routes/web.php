410a0b94faa78dba084185b24df4a9979373f7c8
dangquy <dangquy95php@gmail.com>
2022-06-08 21:28:14 +0700

    add router cache

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return 'Application cache has been cleared';
+});
 
+//Clear route cache:
+Route::get('/route-cache', function() {
+	Artisan::call('route:cache');
+    return 'Routes cache has been cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+ 	Artisan::call('config:cache');
+ 	return 'Config cache has been cleared';
+}); 
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+    return 'View cache has been cleared';
+});
 //Clear Config cache:
 Route::get('/refresh-seed', function() {
     $exitCode = Artisan::call('migrate:refresh --seed');
