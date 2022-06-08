02a7ae1eed94782328827661defe2664edb83424
dangquy <pxwebshop@gmail.com>
2022-06-08 11:30:53 +0700

    up code

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -12,7 +12,7 @@
          <a class="nav-link {{\Request()->route()->getPrefix() != '/account' ? 'collapsed' : '' }}" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
          <i class="bi bi-people"></i><span>Quản Lý Tài khoản</span><i class="bi bi-chevron-down ms-auto"></i>
          </a>
-         <ul id="components-nav" class="nav-content collapse {{\Request()->route()->getPrefix() == '/account' ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
+         <ul id="components-nav" class="nav-content collapse {{\Request()->route()->getPrefix() == 'admin/account' ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
             <li>
                <a href="{{route('list_account')}}" class="{{ \Request::route()->getName() == 'list_account' ? 'active' : ''}}">
                <i class="bi bi-circle"></i><span>Danh sách tài khoản</span>
@@ -38,17 +38,17 @@
          <a class="nav-link {{\Request()->route()->getPrefix() != '/excel' ? 'collapsed' : '' }}" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
          <i class="bi bi-file-earmark-excel"></i><span>Dữ liệu Excel</span><i class="bi bi-chevron-down ms-auto"></i>
          </a>
-         <ul id="tables-nav" class="nav-content collapse {{\Request()->route()->getPrefix() == '/excel' ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
+         <ul id="tables-nav" class="nav-content collapse {{\Request()->route()->getPrefix() == 'admin/excel' ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
             <li>
                <a href="{{route('data_import')}}" class="{{ \Request::route()->getName() == 'data_import' ? 'active' : ''}}">
                <i class="bi bi-circle"></i><span>Nhập dũ liệu Excel</span>
                </a>
             </li>
-            <li>
+            <!-- <li>
                <a href="{{route('data_import_history')}}" class="{{ \Request::route()->getName() == 'data_import_history' ? 'active' : ''}}">
                <i class="bi bi-circle"></i><span>Lịch sử Import</span>
                </a>
-            </li>
+            </li> -->
          </ul>
       </li>
       <!-- --------------------- -->
@@ -56,7 +56,7 @@
          <a class="nav-link {{\Request()->route()->getPrefix() != '/area' ? 'collapsed' : '' }}" data-bs-target="#tables-area" data-bs-toggle="collapse" href="#">
          <i class="bi bi-file-earmark-excel"></i><span>Quản Lý Khu Vực</span><i class="bi bi-chevron-down ms-auto"></i>
          </a>
-         <ul id="tables-area" class="nav-content collapse {{\Request()->route()->getPrefix() == '/area' ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
+         <ul id="tables-area" class="nav-content collapse {{\Request()->route()->getPrefix() == 'admin/area' ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
             <li>
                <a href="{{route('index_area')}}" class="{{ \Request::route()->getName() == 'index_area' ? 'active' : ''}}">
                <i class="bi bi-circle"></i><span>Danh sách khu vực</span>
