aaefe9a850999d649d2f866b61b8f717651c60ec
dangquy <pxwebshop@gmail.com>
2022-06-13 10:13:49 +0700

    UPLOAD

--- database/seeders/AreaSeeder.php
+++ database/seeders/AreaSeeder.php
@@ -16,19 +16,82 @@ class AreaSeeder extends Seeder
         //
          $data = [
             [
-                'name' => 'kv1',
+                'name' => 'THIỆN',
                 'status' => 1,
                 'note' => 'Khu vực có nhiều người trẻ'
             ],
             [
-                'name' => 'kv2',
+                'name' => 'YẾN',
                 'status' => 1,
                 'note' => 'Khu vực có tiềm năng cao'
             ],
             [
-                'name' => 'Bình Chuẩn',
+                'name' => 'TRÚC LINH',
                 'status' => 1,
-                'note' => 'Khu vực đông người đăng ký bảo hiểm'
+                'note' => ''
+            ],
+
+
+            [
+                'name' => 'MÙI',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'TÂM',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'KIM LUYẾN',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'HẰNG',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'VÂN ANH',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'VY',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'THOA',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'LUYÊN',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'HUỲNH LINH',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'MỸ',
+                'status' => 1,
+                'note' => ''
+            ],
+
+            [
+                'name' => 'HUYỀN',
+                'status' => 1,
+                'note' => ''
+            ],
+            [
+                'name' => 'LƯU TUYÊN',
+                'status' => 1,
+                'note' => ''
             ],
         ];
         \DB::table('areas')->insert($data);
