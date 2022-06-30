e126ed18caa2815c9ff72dc3d82c3909f3098b5d
dangquy <pxwebshop@gmail.com>
2022-06-30 15:07:53 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,7 +40,7 @@
         <div class="d-flex align-items-center justify-content-between">
             <a href="/" class="logo d-flex align-items-center">
                 <img src="{{ asset('assets/img/logo.png') }}" alt="">
-                <span class="d-none d-lg-block">Sales Khách Hàng</span>
+                <span class="d-none d-lg-block">Nhân Viên Sales</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div>
