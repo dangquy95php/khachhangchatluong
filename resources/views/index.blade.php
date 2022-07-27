02f673b8139cfd628ab48bdedff88c5446820a04
dangquy <dangquy95php@gmail.com>
2022-07-27 22:45:01 +0700

    add zalo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="zalo-platform-site-verification" content="MjM41Qtg21DS_wWgl_LwSZ_qmNNJX18hDpa" />
     <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
     <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
     <!-- Google Fonts -->
