0deba69dc6b38ef8f399caedfe43b34027b4735a
dangquy <pxwebshop@gmail.com>
2022-08-22 07:55:38 +0700

    remove modal

--- resources/views/account/list.blade.php
+++ resources/views/account/list.blade.php
@@ -62,40 +62,10 @@
                         <td class="text-center">
                            <a class="btn btn-warning text-white" href="{{route('edit_account', $item->id) }}">Sửa</a>
                            @if($item->username != \Auth::user()->username)
-                              <a data-bs-target="#deleteModal{{ $item->id }}" data-bs-toggle="modal"  href="{{route('delete_account', $item->id) }}" class="btn btn-danger">Xoá</button>
+                              <a onclick="return confirm(`Bạn có muốn xóa nhân viên {{ $item->username }} không?`);"  href="{{route('delete_account', $item->id) }}" class="btn btn-danger">Xoá</button>
                             @endif
                         </td>
                      </tr>
-                     <div class="modal fade" id="deleteModal{{ $item->id }}" tabindex="{{$item->id}}" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title">Bạn Có Muốn Xoá Không?</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <p>Tên Đăng Nhập: <b>{{ $item->username }}</b></p>
-                                <p>Email: <b>{{ $item->email }}</b></p>
-                                <p>Trạng Thái:
-                                    @if($item->status == 1)
-                                        <span class="badge bg-success">Đang hoạt động</span>
-                                    @endif
-                                    @if($item->status == 2)
-                                        <span class="badge bg-danger">Không hoạt động</span>
-                                    @endif
-                                    @if($item->status == 0)
-                                        <span class="badge bg-secondary">Chưa kích hoạt</span>
-                                    @endif
-                                </p>
-                                <p>Ngày Đăng Ký: {{ $item->created_at }}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
-                                <a type="button" href="{{route('delete_account', $item->id) }}" class="btn btn-primary">Đồng Ý</a>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
                      @endforeach
                   </tbody>
                </table>
