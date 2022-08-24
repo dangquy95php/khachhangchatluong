795abdf0a0c3a331e773840c97377952f2e080ba
dangquy <pxwebshop@gmail.com>
2022-08-24 14:41:55 +0700

    update mesage

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,6 +9,40 @@ use App\Models\User;
 
 class ReportController extends Controller
 {
+    public static $dataMessage = [[
+            'Bạn Không Có Đơn Nào Để Chốt. Chúng Tôi Cần Sự <span class="text-danger">HÀI HƯỚC</span> Của Bạn Và Sự <span class="text-danger">Cố Gắng</span> Của Bạn Hơn!',
+            'Có Lẽ Hôm Nay Bạn Nhớ Người Yêu Cũ Nên Chưa Gọi Được Khách Nào!',
+            'Khách Hàng Bảo Bạn Hài Hước Và Sẽ Chốt Đơn Với Bạn Sau',
+            'Có Lẽ Trai Lạ Đến Văn Phòng Hay Sao Mà Hiệu Xuất Làm Việc Của Bạn Hơi Yếu!'
+        ], [
+            'Bạn Cần Nổ Lực Thêm Khi Chốt Đơn.',
+            'Chắc Bạn Cũng Mệt Rồi. Thôi! Chuẩn Bị Về Với Tình Nhân Nhé!',
+            'Ngày Nay Bạn Đã Ăn No Chưa. Sức Lực Làm Việc Của Bạn Hơi Chậm',
+            'Bạn Đã Chốt Đơn! Khách Hàng Phản Hồi Bạn Rất Nhiệt Tình'
+        ], [
+            'Bạn Chốt Đơn Hơi Hơi Ít.',
+            'Bạn Vừa Chốt Đơn Vừa Suy Nghĩ Tối Nay Đi Chơi Với Anh Nào Hả!',
+            'Chốt Đơn Ít, Nhưng Vẫn Còn Muốn Chốt Thêm'
+        ], [
+            'Bạn Đã Không Ngừng Gọi Khách Chốt Đơn.',
+            'Lòng Nhiệt Huyết Của Bạn Đã Làm Khách Hàng Phải Chốt Đơn',
+        ], [
+            'Bạn Tuyệt Vời Lắm Luôn Ý.',
+            'Tuyệt Vời Không Thể Gì Tả Nổi',
+            'Bạn Xứng Đáng Được Về Sớm Gặp TÌNH NHÂN.',
+            'Bạn Rất Là Tuyệt Vời! Bạn Có Nôn Nóng Về Gặp Người Yêu Không Mà Chốt Đơn Liên Tục Hay Vậy?'
+        ], [
+            'Bạn Quá Đỉnh! Chốt Đơn Liên Tục.',
+            'Đỉnh Lắm Bạn Ơi! Chốt Đơn Mệt Nghỉ',
+            'Điều Gì Khiến Bạn Chốt Đơn Nhanh Vậy. Cảm Ơn Sự Cống Hiến Của Bạn'
+        ] , [
+            'CẢM ƠN SỰ CỐNG HIẾN CỦA BẠN. BẠN ĐÃ VƯỢT QUÁ MỨC SỰ KÌ VỌNG! QUÁ ĐỈNH.',
+            'CẢM ƠN BẠN RIẾT NHIỀU. BẠN CHỐT ĐƠN QUÁ ĐỈNH, ĐỀ XUẤT BẠN LÊN LÀM QUẢN LÝ :D',
+            'TẠI SAO BẠN GIỎI ĐẾN VẬY. BẠN CÓ BÍ QUYẾT GÌ ĐỀ CHỐT ĐƠN LIÊN TỤC KHÔNG',
+            'BẠN XỨNG ĐÁNG LÊN LƯƠNG, CẢM ƠN NGÀY LÀM VIỆC CỦA BẠN'
+        ]
+    ];
+
     public function index(Request $request)
     {
         $listCallOfStaff = User::with('customers_today_called')->orderBy('username', 'asc')->get();
