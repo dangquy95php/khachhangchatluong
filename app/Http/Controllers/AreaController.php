02aaa0a983b0ac48edfabac25d1dd1443031297e
dangquy95php@gmail.com <dangquy95php@gmail.com>
2022-09-03 17:17:09 +0700

    update

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -158,7 +158,7 @@ class AreaController extends Controller
     public function doleCustomersToArea()
     {
         $areas = Area::with('customers')->orderBy('name', 'ASC')->get();
-        $customers = Customer::whereNull('area_id')->whereNull('called')->get();
+        $customers = Customer::whereNull('area_id')->whereNull('called')->paginate(2000);
 
         return view('area.list-dole', compact('areas', 'customers'));
     }
