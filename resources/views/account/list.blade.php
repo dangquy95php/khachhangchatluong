8f9c41bc2bc1613dc85979a263f054c930392da0
dangquy95php@gmail.com <dangquy95php@gmail.com>
2023-06-05 20:27:02 +0700

    add

--- resources/views/account/list.blade.php
+++ resources/views/account/list.blade.php
@@ -33,7 +33,7 @@
                   </thead>
                   <tbody>
                      @foreach($data as $key => $item)
-                     @if($item->username !== 'dangquy' && $item->username !== 'PHANYEN')
+                     @if($item->username !== 'dangquy')
                      <tr class="{{$item->username == \Auth::user()->username ? 'bg-danger text-white' : '' }}">
                         <th scope="row">{{ ++$key }}</th>
                         <td>{{$item->name}}</td>
